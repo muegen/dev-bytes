618655a21ed5279e866c3ad0187b0a07853c66c2
phillip danner <pbdanner@gmail.com>
2023-05-15 18:46:34 +0000

    structure updates

--- byte-vault/index.js
+++ byte-vault/index.js
@@ -10,19 +10,3 @@ const port = 4000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
-
-// const express = require('express');
-// const app = express();
-// const path = require('path');
-
-// app.get('/', (req, res) => {
-//   const filePath = path.join(__dirname, 'index.js');
-//   console.log(`Serving file: ${filePath}`);
-//   res.sendFile(filePath);
-// });
-
-// const port = 4000;
-
-// app.listen(port, () => {
-//   console.log(`Server started on port ${port}`);
-// });
